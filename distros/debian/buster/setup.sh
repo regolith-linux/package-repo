3aa905329b641d64713d857d928f5ae7f763c1a8
Ken Gilmer <kgilmer@gmail.com>
2021-08-22 19:56:34 -0700

    Avoid PPA install on runner

--- distros/debian/buster/setup.sh
+++ distros/debian/buster/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # This PPA adds nececessary backports for debbuild helper 13
-sudo add-apt-repository -y ppa:videolan/master-daily
+if [ $MACHTYPE = "aarch64-unknown-linux-gnu" ]; then
+    echo "No action on aarch64-unknown-linux-gnu."
+else
+    sudo add-apt-repository -y ppa:videolan/master-daily
+fi
