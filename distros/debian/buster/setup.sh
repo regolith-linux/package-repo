783b76e2fe49d805a00e0819853c530bd0480a56
Ken Gilmer <kgilmer@gmail.com>
2021-12-05 12:56:56 -0800

    Reset manifests and remove old distro versions

--- distros/debian/buster/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This PPA adds nececessary backports for debbuild helper 13
-if [ $MACHTYPE = "aarch64-unknown-linux-gnu" ]; then
-    echo "No action on aarch64-unknown-linux-gnu."
-else
-    sudo add-apt-repository -y ppa:videolan/master-daily
-fi
