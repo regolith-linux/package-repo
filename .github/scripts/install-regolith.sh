a7dc44b1814bcc60b71539118a94f9132cb799d5
Ken Gilmer <kgilmer@gmail.com>
2021-12-13 08:28:00 -0800

    Fix and reneable installation test automation

--- .github/scripts/install-regolith.sh
+++ .github/scripts/install-regolith.sh
@@ -9,7 +9,7 @@ CODENAME=$3
 ARCH=$4
 
 wget -qO - https://regolith-linux.github.io/package-repo/regolith.key | sudo apt-key add -
-echo "deb [arch=amd64] https://regolith-linux.github.io/package-repo/$STAGE/$DISTRO/$CODENAME/$ARCH $CODENAME main" | sudo tee /etc/apt/sources.list.d/regolith.list
+echo "deb [arch=$ARCH] https://regolith-linux.github.io/package-repo/$STAGE/$DISTRO/$CODENAME/$ARCH $CODENAME main" | sudo tee /etc/apt/sources.list.d/regolith.list
 sudo apt update
-sudo apt install -y regolith-desktop
+sudo apt install --dry-run regolith-desktop
 sudo rm /etc/apt/sources.list.d/regolith.list
